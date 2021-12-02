d3c98b7490b8ac6f35188bf7638f107b9db88b75
Connor Roberts <connoobets@gmail.com>
2021-12-02 10:04:37 -0700

    add multiple maps and tables, some default and at least one per user

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,5 +1,8 @@
 INSERT INTO maps (title, center_lat, center_lng, zoom) VALUES ('Downtown Edmonton', 53.547776, -113.493945, 12);
 INSERT INTO maps (title, center_lat, center_lng, zoom) VALUES ('Downtown Vancouver', 49.246222,  -123.101939, 12);
 INSERT INTO maps (title, center_lat, center_lng, zoom, user_id) VALUES ('Downton Regina', 50.438432,  -104.615715, 12, 5);
-INSERT INTO maps (title, center_lat, center_lng, zoom, user_id) VALUES ('Places that are bad', 53.562080, -113.269704, 12, 6);
-
+INSERT INTO maps (title, center_lat, center_lng, zoom, user_id) VALUES ('Places That Are Bad', 53.562080, -113.269704, 12, 6);
+INSERT INTO maps (title, center_lat, center_lng, zoom, user_id) VALUES ('Goose Attacks', 49.305273, -123.138856, 10, 4);
+INSERT INTO maps (title, center_lat, center_lng, zoom, user_id) VALUES ('Places I''ve Sneezed (Nov. 2021)', 51.039140, -114.049071, 12, 4);
+INSERT INTO maps (title, center_lat, center_lng, zoom) VALUES ('Loud Places in Vancouver', 49.278280, -123.116600, 12);
+INSERT INTO maps (title, center_lat, center_lng, zoom) VALUES ('Places I Have Never Been in Toronto', 43.716059, -79.378999, 12);
