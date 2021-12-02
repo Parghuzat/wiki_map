b06ca6cc2a05749696f0f1f9c0333d5059510a86
kunvar13 <kunvar13@gmail.com>
2021-12-01 22:41:16 -0600

    Adding fav function to list

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,5 +1,5 @@
-INSERT INTO maps (title, center_lat, center_lng, zoom) VALUES ('Downtown Edmonton', 53.547776, -113.493945, 12);
-INSERT INTO maps (title, center_lat, center_lng, zoom) VALUES ('Downtown Vancouver', 49.246222,  -123.101939, 12);
-INSERT INTO maps (title, center_lat, center_lng, zoom, user_id) VALUES ('Downton Regina', 50.438432,  -104.615715, 12, 5);
-INSERT INTO maps (title, center_lat, center_lng, zoom, user_id) VALUES ('Places that are bad', 53.562080, -113.269704, 12, 6);
+INSERT INTO maps (title, center_lat, center_lng, zoom, user_id) VALUES ('Downtown Edmonton', 53.547776, -113.493945, 12,1);
+INSERT INTO maps (title, center_lat, center_lng, zoom, user_id) VALUES ('Downtown Vancouver', 49.246222,  -123.101939, 12,2);
+INSERT INTO maps (title, center_lat, center_lng, zoom, user_id) VALUES ('Downton Regina', 50.438432,  -104.615715, 12, 3);
+INSERT INTO maps (title, center_lat, center_lng, zoom, user_id) VALUES ('Places that are bad', 53.562080, -113.269704, 12, 3);
 
